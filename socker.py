322ab5f9a93d2422e2b370f8e53e8a5b1043f140
NishanthChowdary097 <nishanthchowdary118@gmail.com>
2024-11-22 10:49:07 +0530

    modified:   socker.py

--- socker.py
+++ socker.py
@@ -10,7 +10,6 @@ socketio = SocketIO(app)
 clients=set()
 
 def worker(img_path,sid):
-    time.sleep(10)
     with open(img_path, "rb") as image_file:
         image_binary = image_file.read()
     img = base64.b64encode(image_binary).decode("utf-8")
@@ -27,8 +26,7 @@ def handle_message(msg):
     image_data = base64.b64decode(msg['img'])
     with open(f"./images/{msg['user_id']}.{msg['time_stamp']}.png", 'wb') as image_file:
         image_file.write(image_data)
-    socketio.start_background_task(worker,f"./images/{msg['user_id']}.{msg['time_stamp']}.png",request.sid)
-    send(f"Echo: {msg}") 
+    # socketio.start_background_task(worker,f"./images/{msg['user_id']}.{msg['time_stamp']}.png",to=request.sid)
 
 @socketio.on('connect')
 def handle_connect():
